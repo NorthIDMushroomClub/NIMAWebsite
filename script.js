2085a836fad3af654dc48a2f814db5d1694719cc
NorthIDMushroomClub <hopewalton90@gmail.com>
2025-02-06 07:23:15 -0800

    Updated about us/events pages

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+const eventContent = document.getElementById("event__content");
+const eventParagraph = document.createElement("p");
 
+eventParagraph.textContent = "Meetings are over for the season but will resume in the Spring. Check back for updates on the latest news and events!";
+
+
+
+function seasonChange() {
+    const today = new Date();
+    const currentMonth = today.getMonth();
+
+    if(currentMonth < 4 || currentMonth > 10){
+        eventContent.insertBefore(eventParagraph, eventContent.firstChild);
+    }
+}
+
+seasonChange();
